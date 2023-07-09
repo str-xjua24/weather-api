a7abe6578b404374564d6aeca9bccaf6b8c820ee
Jos <jejuarez97@gmail.com>
2023-07-08 20:04:08 -0700

    Load weather api data on the DOM

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const API_KEY = "ef9db5b0c2f04274ae4210353230207";
+const currLocation = document.getElementById("location-name");
+const weatherIcon = document.getElementById("weather-icon");
+const tempFarenheit = document.getElementById("temp-farenheit");
+const tempCelsius = document.getElementById("temp-celsius");
 
 function getWeather() {
   fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=la`)
@@ -7,6 +11,10 @@ function getWeather() {
     })
     .then((res) => {
       console.log(res);
+      currLocation.textContent = `${res.location.name}, ${res.location.region}`;
+      weatherIcon.src = res.current.condition.icon;
+      tempFarenheit.textContent = res.current.temp_f;
+      tempCelsius.textContent = res.current.temp_c;
     })
     .catch((err) => {
       console.log(err);
